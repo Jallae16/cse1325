da86ec8fe9f0bce280d6ac8e96b746c9ace17d2e
Jacob <jtm7980@mavs.uta.edu>
2023-08-31 22:38:46 -0500

    forgot to add a new bonus folder

--- P02/full_credit/Color.java
+++ P02/full_credit/Color.java
@@ -1,18 +1,6 @@
 enum Color{
-	RED(0xFF0000),
-	GREEN(0x00FF00),
-	BLUE(0x0000FF),
-	YELLOW(0xFFFF00);
-
-
-	private final int rbg;
-
-	private Color(int rbg){
-		this.rbg = rbg;
-	}
-
-	@Override
-	public String toString(){
-		return " " + this.name + " " + rbg + " ";
-	}
+	RED,
+	GREEN,
+	BLUE,
+	YELLOW;
 }
