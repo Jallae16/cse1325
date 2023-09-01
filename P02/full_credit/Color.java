023fc3de3b4fd73605724165df772f332ad648b2
Jacob <jtm7980@mavs.uta.edu>
2023-08-31 22:34:10 -0500

    working on bonus

--- P02/full_credit/Color.java
+++ P02/full_credit/Color.java
@@ -1,6 +1,18 @@
 enum Color{
-	RED,
-	GREEN,
-	BLUE,
-	YELLOW
+	RED(0xFF0000),
+	GREEN(0x00FF00),
+	BLUE(0x0000FF),
+	YELLOW(0xFFFF00);
+
+
+	private final int rbg;
+
+	private Color(int rbg){
+		this.rbg = rbg;
+	}
+
+	@Override
+	public String toString(){
+		return " " + this.name + " " + rbg + " ";
+	}
 }
