1fa68283d4796828dc144a6ba97ac7482b8cf25a
Jacob <jtm7980@mavs.uta.edu>
2023-09-09 21:54:03 -0500

    got the foundation working

--- P03/full_credit/Publication.java
+++ P03/full_credit/Publication.java
@@ -30,9 +30,12 @@ public class Publication{
    
    @Override
    public String toString(){
-      return "The Great " + author + " wrote " + title +
-      		 ", with its copyright active since " + copyright + 
-      		 "\n>loaned to the incredible, beautiful, and The GOAT: " + loanedTo + 
-      		 " until they must concede the book back to the library on " + dueDate;
+      StringBuilder pub_toString = new StringBuilder();
+      pub_toString.append("The Great ").append(author).append(" wrote ");
+      pub_toString.append(title).append(", with its copyright active since ");
+      pub_toString.append(copyright);
+      if(loanedTo != null)
+         pub_toString.append("\n>loaned to the incredible, beautiful, and The GOAT: 		 ").append(loanedTo).append(" until they must concede the book back to the 			 library on ").append(dueDate);
+      return pub_toString.toString();
    }
 }
