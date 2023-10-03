a44259c3a86a21afefebae10a58a973c91decd7f
Jacob <jtm7980@mavs.uta.edu>
2023-10-02 21:54:02 -0500

    finished it :3

--- P05/bonus/mdi/LibraryManager.java
+++ P05/bonus/mdi/LibraryManager.java
@@ -7,9 +7,7 @@ import java.util.Scanner;
 import java.io.File;
 import java.nio.file.Files;
 
-public class LibraryManager{
-	
-		
+public class LibraryManager{	
 	
 	public LibraryManager(){
 		//lib = new Library("Meowsier-Kitty Public Library");
@@ -26,6 +24,7 @@ public class LibraryManager{
 		 lib.addPublication(book1);
 		 lib.addPublication(book2);
 		 lib.addPublication(book3);
+		 addVideo(lib);
 	}
 
 	public static void addVideo(Library lib){
@@ -40,9 +39,8 @@ public class LibraryManager{
 	public static void checkOutPublication(Library lib){
 		Scanner scanner = new Scanner(System.in);
 			 
-		int bookNum = Integer.parseInt(System.console().readLine("\nWhat item would you like to checkout: "));
-		int patronInd = Integer.parseInt(System.console().readLine("\nWhich patron are you: "));
-		
+		int bookNum = Integer.parseInt(System.console().readLine("\nWhat item would you like to checkout(0-5): "));
+		int patronInd = Integer.parseInt(System.console().readLine("\nWhich patron are you(0-1): "));
 		lib.checkOut(bookNum, patronInd);
 		
 	}
@@ -64,9 +62,9 @@ public class LibraryManager{
 	}
 	
 	public static void main(String[] args){
-	   boolean test = true;
+	   boolean menuRun = true;
 	   Library lib = new Library("Meowsier-Kitty Public Library");
-	   while(test){
+	   while(menuRun){
 		   try{
 			 System.out.println("-------Main Menu-------");
 			 System.out.println("\nPublication:");
@@ -93,52 +91,13 @@ public class LibraryManager{
 		     else if(selection == 6)
 		     	addPatron(lib);
 		     else if(selection == 0)
-		     	test = false;
+		     	menuRun = false;
 		     else throw new IllegalArgumentException("your selection, " + selection + ", must be between 0 and 6");
-		     /*
-		     
-			 Library lib = new Library("Meowsier-Kitty Public Library");
-			 Publication book1 = new Publication("The Assassination of Fred Hampton", "Jeffrey Haas", 2010);
-			 Publication book2 = new Publication("1984", "George Orwell", 1949);
-			 Publication book3 = new Publication("The Formation of Poltiical Parties and the First National Elections in Russia", "Terence Emmons", 1983);
-			 
-			 Video vid1 = new Video("The search for the saddest punt in the world | Chart Party", "Jon Bois", 2019, 54);
-			 Video vid2 = new Video("Deus Ex: Human Revolution is FINE, And Here's Why", "Harry Brewis", 2022, 213);
-			 Video vid3 = new Video("Fear of Cold", "Jacob Geller", 2022, 47);
-			 
-			 //System.out.println(vid1.toString());
-			 
-			 lib.addPublication(book1);
-			 lib.addPublication(book2);
-			 lib.addPublication(book3);
-			 lib.addPublication(vid1);
-			 lib.addPublication(vid2);
-			 lib.addPublication(vid3);
-			 
-			 System.out.println(lib.toString());
-			 
-			 Scanner scanner = new Scanner(System.in);
-			 
-			 System.out.println("\nWhat item would you like to checkout: ");
-			 int bookNum = scanner.nextInt();
-			 
-			 Patron pat1 = new Patron("Allie", "allie23@gmail.com");
-			 Patron pat2 = new Patron("Alex", "alexC1@gmail.com");
-			 lib.addPatron(pat1);
-			 lib.addPatron(pat2);
-			 System.out.println(lib.patronMenu());
-		 
-			 String buffer = scanner.nextLine();
-			 System.out.println("\nWhich one are you: ");
-			 int patron = scanner.nextInt();
-			 
-			 lib.checkOut(bookNum, patron);
-			 
-			 System.out.println(lib.toString());*/
 			}
 		
 			catch(Exception e){
 			 System.err.println("Unable to checkout a publication\n" + e);
+			 menuRun = false;
 			}
 		}
 	}
