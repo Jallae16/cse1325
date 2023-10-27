858a3b2a96e41dca670db84b6d444ae3d51570a3
Jacob <jtm7980@mavs.uta.edu>
2023-10-27 17:53:36 -0500

    finished bonus

--- P08/bonus/caps.cpp
+++ P08/bonus/caps.cpp
@@ -9,16 +9,18 @@ int main(int args, char* argv[]){
 	std::vector<std::string>* no_caps = new std::vector<std::string>; //on heap
 	
 	for(int i = 1; i < args; i++){
-	    if()
-		caps.push_back(std::string(argv[i]));
+	    if(isupper(std::string(argv[i]).at(0)))
+			caps.push_back(std::string(argv[i]));
+		else
+			no_caps->push_back(std::string(argv[i]));
 	}
 	
 	std::cout << "Capitalized:\n";
 	for(auto v : caps){
 	   std::cout << v << std::endl;
 	}
-	std::cout << "Lower Case:\n";
-	for(auto v : no_caps){
+	std::cout << "\nLower Case:\n";
+	for(auto v : *no_caps){
 	   std::cout << v << std::endl;
 	}
 }
