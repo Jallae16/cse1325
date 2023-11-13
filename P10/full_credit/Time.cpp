840c2aebefef08b7d80f3b0cbe628fddbebf0e79
Jacob <jtm7980@mavs.uta.edu>
2023-11-13 10:18:42 -0600

    working

--- P10/full_credit/Time.cpp
+++ P10/full_credit/Time.cpp
@@ -10,3 +10,27 @@ Time::Time(int hour, int minute, int second)
 	: _hour{hour}, _minute{minute}, _second{second} {
 	
 	}
+
+int Time::compare(const Time& time){
+	if(_hour < time._hour) return -1;
+	if(_hour > time._hour) return 1;
+	if(_minute < time._minute) return -1;
+	if(_minute > time._minute) return 1;
+	if(_second < time._second) return -1;
+	if(_second > time._second) return 1;
+}
+
+std::ostream& operator<<(std::ostream& os, const Time& time){
+	os << time._hour << " " << time._minute << " " << time._second;
+	return os;
+}
+
+std::istream& operator>>(std::istream& is, Time& time){
+	is >> time._hour;
+	is >> time._minute;
+	is >> time._second;
+	if(1 > time._hour || time._hour > 24)
+		throw std::runtime_error("Invalid Hour");
+}
+
+
