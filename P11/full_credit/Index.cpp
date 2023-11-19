03220e6a2f410a110266db0377599ced5cf5e626
Jacob <jtm7980@mavs.uta.edu>
2023-11-18 20:45:28 -0600

    helllp

--- P11/full_credit/Index.cpp
+++ P11/full_credit/Index.cpp
@@ -7,14 +7,15 @@
 
 void Index::add_word(const Word& word, const std::string& filename, int line){
 	Location loc(filename, line);
-	
+	if(_index.find(word) == _index.end())
+		_index[word].insert(loc);
 	_index[word].insert(loc);
 }
 
 std::ostream& operator<<(std::ostream& ost, const Index& index) {
     std::cout << "Index\n=====\n\n" << std::endl;
     
-    for(auto &i : Index._index){
+    for(auto &i : index._index){
     	const Word &word = i.first;
     	const Locations &locations = i.second;
     	
